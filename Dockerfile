e1b9bc8898c1c95f0770f098862331c4c763743c
elderica <1130138+elderica@users.noreply.github.com>
2024-02-24 11:05:06 +0900

    ci: introduce CI with GitHub Actions

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,13 @@
 FROM alpine:latest
 
-ENV QUICKLISP_SHA256SUM=4a7a5c2aebe0716417047854267397e24a44d0cce096127411e9ce9ccfeb2c17
-
 RUN apk update \
  && apk add --no-cache sbcl make
-
-RUN wget -q -O quicklisp.lisp "https://beta.quicklisp.org/quicklisp.lisp" \
- && (sha256sum quicklisp.lisp | grep ${QUICKLISP_SHA256SUM}) \
- && sbcl --non-interactive \
-    --load quicklisp.lisp \
-    --eval '(quicklisp-quickstart:install)' \
-    --eval '(ql-util:without-prompting (ql:add-to-init-file))' \
- && rm quicklisp.lisp
 
 WORKDIR /root/common-lisp/dezero-naive
 
 COPY . .
+
+RUN scripts/install-quicklisp.sh
 
 RUN make quickload
 
---
+++ scripts/install-quicklisp.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -u -e
+
+if  type wget >/dev/null 2>&1
+then
+    wget -q -O quicklisp.lisp "https://beta.quicklisp.org/quicklisp.lisp"
+elif type curl >/dev/null 2>&1
+then
+    curl -sS -o quicklisp.lisp "https://beta.quicklisp.org/quicklisp.lisp"
+else
+    echo 'install wget or curl'
+    exit 1
+fi
+
+sha256sum -c - <<EOF
+4a7a5c2aebe0716417047854267397e24a44d0cce096127411e9ce9ccfeb2c17  quicklisp.lisp
+EOF
+
+LISP=${LISP:-sbcl}
+
+_no_debug_flag=""
+case "${LISP}" in
+    sbcl)
+        _no_debug_flag="--non-interactive"
+        ;;
+esac
+
+${LISP} "${_no_debug_flag}"  \
+        --load quicklisp.lisp \
+        --eval '(quicklisp-quickstart:install)' \
+        --eval '(ql-util:without-prompting (ql:add-to-init-file))'
+
+rm quicklisp.lisp
